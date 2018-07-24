38f364944330ead21b05058258accacb7de3fca8
arik <arikkrol@gmail.com>
2018-07-24 19:06:44 +0300

    script changed

--- build/lawgeex-deploy.sh
+++ build/lawgeex-deploy.sh
@@ -1,6 +1,13 @@
-sudo docker ps #not required - just to prompt the user for sudo password
-grunt
-cd ../word
-sudo docker cp sdk-all-min.js "$1":/var/www/onlyoffice/documentserver/sdkjs/word
-sudo docker cp sdk-all.js "$1":/var/www/onlyoffice/documentserver/sdkjs/word
+
+if [ $# -eq 1 ] 
+then
+   sudo docker ps #not required - just to prompt the user for sudo password
+   grunt
+   cd ../word
+   sudo docker cp sdk-all-min.js "$1":/var/www/onlyoffice/documentserver/sdkjs/word
+   sudo docker cp sdk-all.js "$1":/var/www/onlyoffice/documentserver/sdkjs/word
+else
+    echo "execution failed. required args: <container-id>"
+fi
+
 
